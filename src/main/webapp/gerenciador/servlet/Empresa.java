f59ceae0d6884a45d37066f707bfac951b9c966a
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2022-11-11 09:49:07 -0300

    Feat: adicionando arquivos

---
+++ src/main/webapp/gerenciador/servlet/Empresa.java
@@ -0,0 +1,32 @@
+package gerenciador.servlet;
+
+import java.util.Date;
+
+public class Empresa {
+	
+	private int id;
+	private String Nome;
+	private Date dataAbertura = new Date();
+	
+	public int getId() {
+		
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+	
+	
+}
